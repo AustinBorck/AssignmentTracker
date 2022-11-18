73d844c20d213bd418908723ebd1c1ac5ed18a12
Austin Borck <austinborck98@gmail.com>
2022-11-18 11:46:35 -0700

    added some styling

--- view/header.php
+++ view/header.php
@@ -5,6 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Assignment Tracker</title>
+        <link rel="stylesheet" href="sass/main.css">
     </head>
 <body>
     <main class="main">
